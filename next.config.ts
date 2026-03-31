0e3d843a4502edd07f6742954376584551b35dd7
Karmanya Singh <karmanyasingh8@gmail.com>
2026-03-31 18:38:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
-  },
+
 };
 
 export default nextConfig;
